5291fb1feec4f50efc67b244b0a6bba5c945e2aa
AlexisArmandoTrejoMartinez <alexisarmando456@hotmail.com>
2020-05-11 21:43:14 -0500

    Algoritmo Busqueda en Anchura

--- Anchura.py
+++ Anchura.py
@@ -17,18 +17,56 @@ def primeroAnchura(Carpeta,Archivo)P:
 # Trejo Martinez Alexis Armando
 
 import json
-with open('base(2).json') as file:
+with open('base3.json') as file:
 	data = json.load(file)
-colaangenda = []
-expandido = []
-visitados =[]
+revisados =[]
+camino=[]
+auxiliar = []
+Nodsig=[]
 
-def hijos(padres):
-	if padres in data[1]:
-		print(colaangenda[0])
-		return colaangenda
+def primeroAnchura(Carpeta,Archivo):
+	#Recorrido Siguiente del Nodo Principal 
+	if Carpeta == Archivo:
+		return Archivo
+	if Nodsig:
+		contador = 0
+		for X in Nodsig:
+			 contador += 1
+		if contador > 1:
+			print("Nodo principal: " +Nodsig[0])
+			Nodsig.pop(0)#Se realiza la eliminación de la cola adyacente
+			#Se genera un auxiliar para los nodos ya visitados y asu vez se limpia
+	if auxiliar:
+		del auxiliar[:]
+	for i in data:
+		if i[0] == Carpeta:
+			Nodsig.append(i[1])
+			auxiliar.append(i[0])
+			if i[1] == Archivo:
+				nodo = primeroAnchura(i[1],Archivo)#Se genera la primera recursividad
+				return nodo
+	revisados.append(list(set(auxiliar)))
+	if revisados:
+		print("Nodos Recorridos Adyacentemente")
+		print(str(revisados))
+		print(Nodsig[0])
+		camino.append(Nodsig[0])
+		return primeroAnchura(Nodsig[0],Archivo)
 
-def primeroAnchura(carpeta,archivo):
+nodo = primeroAnchura("C:","Profe de quimica en arrel.jpg")
+print("Hijo encontrado en el nodo principal: " +nodo)
+cam = []
+if nodo:
+	for c in camino:
+		if c not in cam:
+			cam.append(c)
+	print("Padres(Nodos Revisados)")
+	print(cam)
+else:
+	print("No se encontro")
+
+
+"""def primeroAnchura(carpeta,archivo):
 	if carpeta == archivo:
 		return archivo
 
@@ -43,4 +81,4 @@ def primeroAnchura(carpeta,archivo):
 				return r
 primeroAnchura("1","20")
 
-print(expandido)
+print(expandido)"""
